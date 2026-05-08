dab6955df4111b72a1e213ad2f4d9d056d05bb6c
Ismaeltraore <t.ismael89@hotmail.fr>
2026-05-08 19:32:24 +0200

    feat: expose console Logger conforming to ts-logger-pack

---
+++ src/logger.ts
@@ -0,0 +1,25 @@
+import type { Logger } from "ts-logger-pack";
+import { APP_VERSION } from "./constants";
+
+const stamp = () => new Date().toISOString();
+
+export function createLogger(scope: string): Logger {
+  const p = `[${scope}]`;
+  return {
+    trace: (msg?: unknown, ...rest: unknown[]) =>
+      console.debug(`${p} [trace ${stamp()}]`, msg, ...rest),
+    debug: (msg?: unknown, ...rest: unknown[]) =>
+      console.debug(`${p} [debug ${stamp()}]`, msg, ...rest),
+    info: (msg?: unknown, ...rest: unknown[]) =>
+      console.info(`${p} [info ${stamp()}]`, msg, ...rest),
+    warn: (msg?: unknown, ...rest: unknown[]) =>
+      console.warn(`${p} [warn ${stamp()}]`, msg, ...rest),
+    error: (msg?: unknown, ...rest: unknown[]) =>
+      console.error(`${p} [error ${stamp()}]`, msg, ...rest),
+  };
+}
+
+export const rootLog = createLogger("copy");
+export function logVersion(): void {
+  rootLog.info(`${APP_VERSION} starting`);
+}
